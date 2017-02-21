39bf6e29da338ec19ffc5e42163b3d6f97c711d0
Bhupesh Gupta <bhupesh.gupta@sourcefuse.com>
2017-02-21 11:15:43 +0530

    removed access keys of AWS

--- app/Controller/Component/AwsComponent.php
+++ app/Controller/Component/AwsComponent.php
@@ -6,61 +6,60 @@ use Aws\S3\S3Client;
 
 class AwsComponent extends Component
 {
-    
     /**
-    * @var : name of bucket in which we are going to operate
-    */
+     * @var : name of bucket in which we are going to operate
+     */
     private $bucket = 'bgsf';
 
     /**
-    * @var : Amazon S3Client object
-    */
+     * @var : Amazon S3Client object
+     */
     private $s3 = null;
-    
-    
+
     public function __construct()
     {
         $this->s3 = S3Client::factory(array(
             'credentials' => array(
-                'key' => 'AKIAJVPAWG5HXN65EXVQ',
-                'secret'  => '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',
+                'key' => '',
+                'secret' => '',
             ),
             'region' => 'ap-south-1',
             'version' => 'latest',
-            'signature_version' => 'v4'
+            'signature_version' => 'v4',
         ));
     }
-    
-    
+
     /**
      * @desc : to upload file on bucket with specified path
+     *
      * @param : keyname > path of file which need to be uploaded
-     * @return : uploaded file object 
+     *
+     * @return : uploaded file object
      * @created on : 14.03.2014
-    */ 
+     */
     public function upload($sourceFile, $fileKey, $isPublic = 0)
     {
         try {
             $acl = 'private';
-            if ($isPublic !=0) {
+            if ($isPublic != 0) {
                 $acl = 'public-read';
             }
-            
+
             $ext = pathinfo($fileKey, PATHINFO_EXTENSION);
             $contentType = $this->getMimeContentType($ext);
             $this->s3->putObject(array(
-                'Bucket'     => $this->bucket,
-                'Key'        => $fileKey,
+                'Bucket' => $this->bucket,
+                'Key' => $fileKey,
                 'SourceFile' => $sourceFile,
-                'ACL'        => $acl,
-                'ContentType'=> $contentType,
+                'ACL' => $acl,
+                'ContentType' => $contentType,
                 'ContentDisposition' => 'attachment;filename='.$fileKey,
             ));
             $this->s3->waitUntil('ObjectExists', array(
                 'Bucket' => $this->bucket,
-                'Key'    => $fileKey
+                'Key' => $fileKey,
             ));
-            
+
             // Get cdnUrl for uploaded object.
             // $cdnUrl = $this->getObjectUrl($fileKey, $isPublic);
             return true;
@@ -70,10 +69,12 @@ class AwsComponent extends Component
     }
 
     /**
-     * It gets the private or public url of the file from S3
-     * @param  [type] $fileKey  file to be fetched
-     * @param  [type] $isPublic file is public or private
-     * @return [string]         S3 url of file
+     * It gets the private or public url of the file from S3.
+     *
+     * @param [type] $fileKey  file to be fetched
+     * @param [type] $isPublic file is public or private
+     *
+     * @return [string] S3 url of file
      */
     public function getObjectUrl($fileKey, $isPublic = 0)
     {
@@ -81,7 +82,7 @@ class AwsComponent extends Component
         if ($isPublic == 0) {
             $cmd = $this->s3->getCommand('GetObject', [
                     'Bucket' => $this->bucket,
-                    'Key'    => $fileKey,
+                    'Key' => $fileKey,
                     'request-content-disposition' => 'attachment;finename='.$fileKey,
                 ]);
 
@@ -91,37 +92,38 @@ class AwsComponent extends Component
         } else {
             $objUrl = $this->s3->getObjectUrl($this->bucket, $fileKey);
         }
-        
+
         return $objUrl;
     }
 
     /**
      * This function returns mimeContentType.
-     * 
+     *
      * @param string $ext File Extension
+     *
      * @return string MIME Content Type
      */
     private function getMimeContentType($ext)
     {
         $contentType = 'application/octet-stream';
-        
-        $mimeTypes = array (
-            'txt'   => 'text/plain',
-            'json'  => 'application/json',
-            'xml'   => 'text/xml',
+
+        $mimeTypes = array(
+            'txt' => 'text/plain',
+            'json' => 'application/json',
+            'xml' => 'text/xml',
 
             // images
-            'png'   => 'image/png',
-            'jpe'   => 'image/jpeg',
-            'jpeg'  => 'image/jpeg',
-            'jpg'   => 'image/jpeg',
-            'gif'   => 'image/gif',
-            'bmp'   => 'image/bmp',
-            'ico'   => 'image/vnd.microsoft.icon',
-            'tiff'  => 'image/tiff',
-            'tif'   => 'image/tiff',
-            'svg'   => 'image/svg+xml',
-            'svgz'  => 'image/svg+xml',
+            'png' => 'image/png',
+            'jpe' => 'image/jpeg',
+            'jpeg' => 'image/jpeg',
+            'jpg' => 'image/jpeg',
+            'gif' => 'image/gif',
+            'bmp' => 'image/bmp',
+            'ico' => 'image/vnd.microsoft.icon',
+            'tiff' => 'image/tiff',
+            'tif' => 'image/tiff',
+            'svg' => 'image/svg+xml',
+            'svgz' => 'image/svg+xml',
 
             // archives
             /*'zip'   => 'application/zip',
@@ -137,74 +139,78 @@ class AwsComponent extends Component
             'psd'   => 'image/vnd.adobe.photoshop',*/
 
             // ms office
-            'doc'   => 'application/msword',
-            'docx'  => 'application/msword',
-            'rtf'   => 'application/rtf',
-            'xls'   => 'application/vnd.ms-excel',
-            'xlsx'  => 'application/vnd.ms-excel',
-            'ppt'   => 'application/vnd.ms-powerpoint',
+            'doc' => 'application/msword',
+            'docx' => 'application/msword',
+            'rtf' => 'application/rtf',
+            'xls' => 'application/vnd.ms-excel',
+            'xlsx' => 'application/vnd.ms-excel',
+            'ppt' => 'application/vnd.ms-powerpoint',
 
             // open office
-            'odt'   => 'application/vnd.oasis.opendocument.text',
-            'ods'   => 'application/vnd.oasis.opendocument.spreadsheet',
+            'odt' => 'application/vnd.oasis.opendocument.text',
+            'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
         );
-        
+
         $ext = strtolower($ext);
         if (isset($mimeTypes[$ext])) {
             $contentType = $mimeTypes[$ext];
         }
-        
+
         return $contentType;
     }
-    
-    
+
     /**
      * @desc : to delete multiple objects from bucket
+     *
      * @param : array(
-                    array('Key' => $keyname1),
-                    array('Key' => $keyname2),
-                    array('Key' => $keyname3),
-                )
+     )
      * @return : boolean
-     * @created on : 14.03.2014   
-    */
+     * @created on : 14.03.2014
+     */
     public function delete($fileKey)
     {
         try {
             return $this->s3->deleteObject(array(
                 'Bucket' => $this->bucket,
-                'Key' => $fileKey
+                'Key' => $fileKey,
             ));
         } catch (RuntimeException $e) {
-            if(Configure::read('debug')) echo 'RuntimeException Exception :'.$e->getMessage() ;
+            if (Configure::read('debug')) {
+                echo 'RuntimeException Exception :'.$e->getMessage();
+            }
         } catch (Exception $e) {
-            if(Configure::read('debug')) echo 'Exception :'.$e->getMessage() ;          
+            if (Configure::read('debug')) {
+                echo 'Exception :'.$e->getMessage();
+            }
         }
+
         return false;
     }
 
     /**
-     * This function for download S3 file Content
-     * 
-     * @param string  $key
-     * @param boolean $isPublic
-     * @param string  $saveAs
+     * This function for download S3 file Content.
+     *
+     * @param string $key
+     * @param bool   $isPublic
+     * @param string $saveAs
+     *
      * @return string
      */
-    public function downloadFile($key, $isPublic=0, $saveAs=null)
+    public function downloadFile($key, $isPublic = 0, $saveAs = null)
     {
         $client = $this->getS3Client();
-        
+
         $filter = array(
             'Bucket' => $this->bucketName,
-            'Key'    => $key
+            'Key' => $key,
         );
-        
+
         if (!is_null($saveAs)) {
             $filter['SaveAs'] = $saveAs;
         }
-        
+
         $obj = $client->getObject($filter);
+
         return $obj['Body'];
     }
 }
