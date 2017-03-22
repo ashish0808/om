7f37e2522727a4c7f4a9b369f5a7e225f069306c
Ashish Chopra <ashish@web-flight.nl>
2017-03-22 09:47:38 +0530

    Fix

--- app/Controller/CasesController.php
+++ app/Controller/CasesController.php
@@ -17,7 +17,6 @@ class CasesController extends AppController
 
 	public function manage($listType = '')
 	{
-		echo Router::url( $this->referer(), true ); die;
 		if ($this->request->isAjax()) {
 			$this->layout = 'ajax';
 		} else {
@@ -115,87 +114,6 @@ class CasesController extends AppController
 
 		$this->set('listType', $listType);
 		$this->set('records', $records);
-	}
-
-	public function index()
-	{
-		if ($this->RequestHandler->isAjax()) {
-			$this->layout = 'ajax';
-		} else {
-			$this->layout = 'basic';
-		}
-
-		$this->loadModel('ClientCase');
-
-		$this->pageTitle = 'Manage Cases';
-		$this->set("pageTitle", $this->pageTitle);
-
-		$fields = array();
-
-		if (isset($this->data['ClientCase']['stage']) && !empty($this->data['ClientCase']['stage'])) {
-			$this->bulkAction($this->data['ClientCase']['stage']);
-		}
-
-		$paginateLimit = LIMIT;
-		if (isset($this->request->data) && !empty($this->request->data)) {
-			foreach ($this->request->data['ClientCase'] as $key => $value) {
-				if (!empty($value)) {
-					$this->request->params['named'][$key] = base64_encode($value);
-				}
-			}
-		}
-
-		if (isset($this->request->params['named']) && !empty($this->request->params['named'])) {
-			foreach ($this->request->params['named'] as $key => $value) {
-				if (!empty($value)) {
-					$this->request->data['ClientCase'][$key] = base64_decode($value);
-					if ($key == 'paging_limit') {
-						$paginateLimit = base64_decode($value);
-					}
-				}
-			}
-		}
-
-		$criteria = "";
-		$criteria .= "1=1";
-		if (isset($this->data) || !empty($this->params['named'])) {
-			if (isset($this->data['ClientCase']['client_name']) && ($this->data['ClientCase']['client_name'] != '')) {
-				$criteria .= " AND (ClientCase.client_first_name LIKE '%" . $this->data['ClientCase']['client_name'] . "%' OR ClientCase.client_last_name LIKE '%" . $this->data['ClientCase']['client_name'] . "%')";
-			} elseif (isset($this->params['named']['client_name']) && $this->params['named']['client_name'] != '') {
-				if (!isset($this->data['ClientCase']['client_name'])) {
-					$criteria .= " AND (ClientCase.first_name LIKE '%" . $this->params['named']['client_name'] . "%' OR ClientCase.last_name LIKE '%" . $this->params['named']['client_name'] . "%')";
-				}
-			}
-
-			if (isset($this->data['ClientCase']['client_email']) && ($this->data['ClientCase']['client_email'] != '')) {
-				$criteria .= " AND (ClientCase.client_email LIKE '%" . $this->data['ClientCase']['client_email'] . "%')";
-			} elseif (isset($this->params['named']['client_email']) && $this->params['named']['client_email'] != '') {
-				if (!isset($this->data['ClientCase']['client_email'])) {
-					$criteria .= " AND (ClientCase.client_email LIKE '%" . $this->params['named']['client_email'] . "%')";
-				}
-			}
-
-			if (isset($this->data['ClientCase']['opponent_name']) && ($this->data['ClientCase']['opponent_name'] != '')) {
-				$criteria .= " AND (ClientCase.opponent_first_name LIKE '%" . $this->data['ClientCase']['opponent_name'] . "%' OR ClientCase.opponent_last_name LIKE '%" . $this->data['ClientCase']['opponent_name'] . "%')";
-			} elseif (isset($this->params['named']['opponent_name']) && $this->params['named']['opponent_name'] != '') {
-				if (!isset($this->data['ClientCase']['opponent_name'])) {
-					$criteria .= " AND (ClientCase.first_name LIKE '%" . $this->params['named']['opponent_name'] . "%' OR ClientCase.last_name LIKE '%" . $this->params['named']['opponent_name'] . "%')";
-				}
-			}
-		}
-
-		$this->Paginator->settings = array(
-			'conditions' => array('ClientCase.is_deleted' => 0),
-			'page' => 1,
-			'limit' => $paginateLimit,
-			'fields' => $fields,
-			'order' => array('ClientCase.id' => 'desc'),
-			//'contain' => array('Customer' => array('Country', 'State', 'City'))
-		);
-		$records = $this->Paginator->paginate('ClientCase', $criteria);
-
-		$this->set('records', $records);
-		$this->set('paginateLimit', $paginateLimit);
 	}
 
 	public function add()
